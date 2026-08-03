23708fc4b6abc2f3db6b3c06975fb206a003a760
Eyad Makkawi <eyadmsi770@gmail.com>
2026-08-04 00:24:23 +0300

    done from Problem2

---
+++ Problem2.cpp
@@ -0,0 +1,59 @@
+/* 
+Write a Program to print all prime numbers from 1 to N
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+enum enPrimeNotPrime { Prime = 1, NotPrime = 2};
+
+float ReadPositiveNumber(string Message)
+{
+    int Number = 0;
+
+    do
+    {
+        cout << Message << endl;
+        cin >> Number;
+    } while (Number <= 0);
+
+    return Number;
+}
+
+
+enPrimeNotPrime CheckPrime(int Number)
+{
+    int M = round(Number / 2);
+    for(int Counter = 2; Counter <= M; Counter++)
+    {
+        if(Number % Counter == 0)
+        return enPrimeNotPrime::NotPrime;
+    }
+    return enPrimeNotPrime::Prime;
+}
+
+void PrintPrimeNumbers(int Number)
+{
+    cout << "\n";
+
+    cout << "Prime Numbers From " << 1 << " To " << Number;
+    cout << " are: " << endl;
+
+    for(int i = 1; i <= Number; i++)
+    {
+        if(CheckPrime(i) == enPrimeNotPrime::Prime)
+        {
+            cout << i << endl;
+        }
+    }
+}
+
+
+int main()
+{
+    PrintPrimeNumbers(ReadPositiveNumber("Please Enter a Positive Number: "));
+    
+    return 0;
+
+}
